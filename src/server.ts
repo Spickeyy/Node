fd48227e238b66da2c4fa521d0e7f37078d2c1c0
Spickeyy <mindegrigas@gmail.com>
2023-02-20 16:40:13 +0200

    feat(): eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import express from 'express';
 import morgan from 'morgan';
 
 
+
+
 const {SERVER_PORT, SERVER_DOMAIN} = process.env;
 
 if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
@@ -17,4 +19,4 @@ server.use(express.static('public'));
 
 server.listen(SERVER_PORT, () => {
   console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`)
-})
+});
