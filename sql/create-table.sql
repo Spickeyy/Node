e6cb1292d4e7b1108d1e515216669a21585d8f29
Spickeyy <mindegrigas@gmail.com>
2023-02-24 09:33:52 +0200

    feat(): added delete table example

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -3,5 +3,7 @@ create table  users(
     email varchar(64),
     password varchar(64),
     firstName varchar(64),
-    lastName varchar(64)
+    lastName varchar(64),
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
