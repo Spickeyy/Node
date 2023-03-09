91182f727e134a670204a13d5a8d0d6290b6439b
Spickeyy <mindegrigas@gmail.com>
2023-03-09 19:02:22 +0200

    chore(): TS reduce example (not readable, but interesting)

--- src/controllers/movies-controller/movies-model/update-movie.ts
+++ src/controllers/movies-controller/movies-model/update-movie.ts
@@ -4,6 +4,67 @@ import { colonObjectQueryFormat } from '../../../services/my-sql';
 import { MovieViewModel, PartialMovieData } from '../types';
 import SQL from './sql';
 
+type PrepareSqlResult = [ string, Record<string, string>];
+
+type PrepareSql = (
+  movieData: PartialMovieData
+  ) => PrepareSqlResult;
+
+const prepareImagesSql: PrepareSql = (movieData) => {
+  const bindingsOrNull = movieData.images?.reduce((prevBindings, img, i) => ({
+    ...prevBindings,
+    [`img${i + 1}`]: img,
+  }), {} as Record<string, string>) ?? null;
+  const shouldInsert = bindingsOrNull !== null;
+  const shouldInsertImages = movieData.images !== undefined && movieData.images.length > 0;
+
+  const sql = shouldInsert
+    ? `
+      DELETE FROM images 
+      WHERE images.movieId = :id;
+    
+      ${shouldInsertImages ? `INSERT INTO images (src, movieId) VALUES
+        ${Object.keys(bindingsOrNull).map((imgBinding) => `(:${imgBinding}, :id)`).join(',\n')};`
+      : ''}
+    ` : '';
+
+    const bindings = bindingsOrNull ?? {};
+  return [sql, bindings];
+};
+
+const prepateLocationSql: PrepareSql = (movieData) => {
+  const sql = movieData.location !== undefined ? `
+    INSERT INTO locations (country) VALUES
+    (:country);` : '';
+  const bindings = movieData.location ?? {};
+
+    return [sql, bindings];
+};
+
+const prepateMovieSql: PrepareSql = (movieData) => {
+  const propsSql = [
+    movieData.title !== undefined ? 'title = :title' : null,
+    movieData.rating !== undefined ? 'rating = :rating' : null,
+    movieData.price !== undefined ? 'price = :price' : null,
+    movieData.location !== undefined ? 'locationId = LAST_INSERT_ID()' : null,
+  ].filter((setPropSql) => setPropSql !== null).join(',\n');
+
+  const sql = propsSql.length > 0 ? `
+    UPDATE movies SET
+    ${propsSql}
+    WHERE movies.id = :id;
+    ` : '';
+
+  const bindings: Record<string, string> = {};
+    if (movieData.title !== undefined) bindings.title = movieData.title;
+    if (movieData.rating !== undefined) bindings.rating = String(movieData.rating);
+    if (movieData.price !== undefined) bindings.price = String(movieData.price);
+
+  return [sql, bindings];
+};
+
+// const prepareSqlArr = [prepateMovieSql, prepateLocationSql, prepareImagesSql];
+
 export const updateMovie = async (
     id: string,
     movieData: PartialMovieData,
@@ -11,45 +72,26 @@ export const updateMovie = async (
     const mySqlConnection = await mysql.createConnection(config.db);
     mySqlConnection.config.queryFormat = colonObjectQueryFormat;
 
-    // Images SQL
-    const imagesBindings = movieData.images?.reduce((prevBindings, img, i) => ({
-      ...prevBindings,
-      [`img${i + 1}`]: img,
-    }), {} as Record<string, string>) ?? null;
-    const shouldAddNewImages = movieData.images !== undefined && movieData.images.length > 0;
-    const imagesUpdatePreparedSql = imagesBindings !== null
-      ? `
-        DELETE FROM images 
-        WHERE images.movieId = :id;
-      
-        ${shouldAddNewImages ? `INSERT INTO images (src, movieId) VALUES
-          ${Object.keys(imagesBindings).map((imgBinding) => `(:${imgBinding}, :id)`).join(',\n')};`
-        : ''}
-      ` : '';
+//     const [preparedSql, bindings] = prepareSqlArr.reduce<PreparationResult>(
+//       ([prevSql, prevBindings], prepareSql) => {
+//         const [sql, binds] = prepareSql(movieData);
 
-    // Location SQL
-    const locationExist = movieData.location !== undefined;
-    const locationInsertSql = locationExist ? `
-      INSERT INTO locations (country) VALUES
-      (:country);` : '';
+//         return [
+//           sql + prevSql,
+//           { ...prevBindings, ...binds },
+//         ];
+//       },
+//       [`${SQL.SELECT} WHERE m.id = :id ${SQL.GROUP}`, { id }],
+// );
 
-    // Property SQL
-    const movieSetPropsSql = [
-      movieData.title !== undefined ? 'title = :title' : null,
-      movieData.rating !== undefined ? 'rating = :rating' : null,
-      movieData.price !== undefined ? 'price = :price' : null,
-      locationExist ? 'locationId = LAST_INSERT_ID()' : null,
-    ].filter((setPropSql) => setPropSql !== null).join(',\n');
+    const [imagesSql, imagesBindings] = prepareImagesSql(movieData);
+    const [locationSql, locationBindings] = prepateLocationSql(movieData);
+    const [movieSql, movieBindings] = prepateMovieSql(movieData);
 
     const preparedSql = `
-      ${imagesUpdatePreparedSql}
-      ${locationInsertSql}
-      ${movieSetPropsSql.length > 0 ? `
-      UPDATE movies SET
-        ${movieSetPropsSql}
-        WHERE movies.id = :id;
-      ` : ''}
-      
+      ${imagesSql}
+      ${locationSql}
+      ${movieSql}
       ${SQL.SELECT}
       WHERE m.id = :id
       ${SQL.GROUP}
@@ -58,10 +100,8 @@ export const updateMovie = async (
     const bindings = {
       id,
       ...imagesBindings,
-      ...movieData.location,
-      title: movieData.title,
-      rating: movieData.rating,
-      price: movieData.price,
+      ...locationBindings,
+      ...movieBindings,
     };
 
     const [queryResultsArr] = await mySqlConnection.query<MovieViewModel[]>(preparedSql, bindings);
